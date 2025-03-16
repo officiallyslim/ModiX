6d7150b7407ac444cb365bac206e9bca33c96131
Slim <officiallyslim123@gmail.com>
2025-03-16 07:29:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,5 @@ updateCountdown();
 
 // Dropdown Menu Toggle
 function toggleMenu() {
-    const menu = document.getElementById("dropdownMenu");
-    menu.classList.toggle("active");
+    document.getElementById("dropdownMenu").classList.toggle("active");
 }
